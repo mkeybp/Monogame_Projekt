abdc0c6176c21c859a8dd7ce73affb4c547f03e1
mikey pedersen <mikeypedersen@hotmail.com>
2019-11-05 17:01:27 +0100

    First Commit

---
+++ GameWorld.cs
@@ -0,0 +1,115 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System.Collections.Generic;
+
+namespace Monogame_Projekt
+{
+    /// <summary>
+    /// This is the main type for your game.
+    /// </summary>
+    public class GameWorld : Game
+    {
+
+        int screenWidth;
+        int screenHeight;
+
+        Texture2D maleSprite;
+
+        private List<GameObject> gameObjects = new List<GameObject>();
+
+        public static Vector2 screenSize;
+
+        
+
+
+
+
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+
+        public GameWorld()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+
+        }
+
+        /// <summary>
+        /// Allows the game to perform any initialization it needs to before starting to run.
+        /// This is where it can query for any required services and load any non-graphic
+        /// related content.  Calling base.Initialize will enumerate through any components
+        /// and initialize them as well.
+        /// </summary>
+        protected override void Initialize()
+        {
+            graphics.PreferredBackBufferWidth = 1000;
+            graphics.PreferredBackBufferHeight = 1000;
+            graphics.IsFullScreen = false;
+            graphics.ApplyChanges();
+            Window.Title = "MonogameProject";
+
+            base.Initialize();
+        }
+
+        /// <summary>
+        /// LoadContent will be called once per game and is the place to load
+        /// all of your content.
+        /// </summary>
+        protected override void LoadContent()
+        {
+            // Create a new SpriteBatch, which can be used to draw textures.
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            maleSprite = Content.Load<Texture2D>("player_fwd");
+
+            // TODO: use this.Content to load your game content here
+        }
+
+        /// <summary>
+        /// UnloadContent will be called once per game and is the place to unload
+        /// game-specific content.
+        /// </summary>
+        protected override void UnloadContent()
+        {
+            // TODO: Unload any non ContentManager content here
+        }
+
+        /// <summary>
+        /// Allows the game to run logic such as updating the world,
+        /// checking for collisions, gathering input, and playing audio.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            // TODO: Add your update logic here
+
+            base.Update(gameTime);
+        }
+
+        /// <summary>
+        /// This is called when the game should draw itself.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            // TODO: Add your drawing code here
+            spriteBatch.Begin();
+            DrawPlayer();
+            spriteBatch.End();
+            base.Draw(gameTime);
+        }
+
+        private void DrawPlayer()
+        {
+
+            spriteBatch.Draw(maleSprite, new Vector2(1, 2), Color.White);
+
+        }
+    }
+}
---
+++ Player.cs
@@ -0,0 +1,130 @@
+﻿using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Monogame_Projekt
+{
+    class Player : GameObject
+    {
+        //protected List<int> weapons = new List<int>;
+        public int playerHealth;
+        public int score;
+        public float speed;
+
+        public Player()
+        {
+            speed = 500f;
+        }
+        /// <summary>
+        /// Loader spiller sprites.
+        /// </summary>
+        /// <param name="content"></param>
+        public override void LoadContent(ContentManager content)
+        {
+            sprites = new Texture2D[4];
+
+        }
+        /// <summary>
+        /// Tjekker for inputs hver frame.
+        /// </summary>
+        /// <param name="gameTime"></param>
+        public override void Update(GameTime gameTime)
+        {
+            HandleInput();
+            Move(gameTime);
+            HandleScore();
+            SelectWeapon();
+            CameraFollow();
+        }
+        /// <summary>
+        /// Score.
+        /// </summary>
+        private void HandleScore()
+        {
+
+        }
+        /// <summary>
+        /// Bevæger spilleren når man trykker på de givne taster.
+        /// </summary>
+        private void HandleInput()
+        {
+            velocity = Vector2.Zero;
+
+            KeyboardState keyState = Keyboard.GetState();
+
+            if (keyState.IsKeyDown(Keys.W))
+            {
+                velocity += new Vector2(0, -1);
+            }
+            if (keyState.IsKeyDown(Keys.A))
+            {
+                velocity += new Vector2(-1, 0);
+            }
+            if (keyState.IsKeyDown(Keys.S))
+            {
+                velocity += new Vector2(0, 1);
+            }
+            if (keyState.IsKeyDown(Keys.D))
+            {
+                velocity += new Vector2(1, 0);
+            }
+            if (velocity != Vector2.Zero)
+            {
+                velocity.Normalize();
+            }
+        }
+        /// <summary>
+        /// Gør at man går samme hastighed ligegyldigt at fps.
+        /// </summary>
+        /// <param name="gameTime"></param>
+        protected void Move(GameTime gameTime)
+        {
+            float deltaTime = (float)gameTime.ElapsedGameTime.TotalSeconds;
+
+            position += ((velocity * speed) * deltaTime);
+        }
+        /// <summary>
+        /// Gør at kameraet følger spilleren.
+        /// </summary>
+        private void CameraFollow()
+        {
+
+        }
+        /// <summary>
+        /// Lader dig skifte våben
+        /// </summary>
+        private void SelectWeapon()
+        {
+
+        }
+        /// <summary>
+        /// Lader dig gå ud fra siden af mappet og komme ind på den anden side.
+        /// </summary>
+        /*private void ScreenWarp()
+        {
+            if (position.X > GameWorld.ScreenSize.X + sprite.Width)
+            {
+                position.X = -sprite.Width;
+            }
+            else if (position.X < -sprite.Width)
+            {
+                position.X = GameWorld.ScreenSize.X + sprite.Width;
+            }
+            if (position.Y > GameWorld.ScreenSize.Y + sprite.Height)
+            {
+                position.Y = -sprite.Height;
+            }
+            else if (position.Y < -sprite.Height)
+            {
+                position.Y = GameWorld.ScreenSize.Y + sprite.Height;
+            }
+        }*/
+    }
+}
